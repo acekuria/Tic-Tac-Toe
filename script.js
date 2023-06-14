d7ebae21bf0f570eeef982be115dc090217ee0c8
AlvinKuria <alvinkuria18@gmail.com>
2023-06-14 16:38:46 +0100

    checkWinner function complete

--- script.js
+++ script.js
@@ -47,16 +47,48 @@ let gameBoardModule = (() => {
         }
         else {
           playerAddMarks(index)
+          checkWinner()
         }
         });
     })
   }
 
-  return {avoidSameSquare,gameBoard,};
+  let checkWinner = function() {
+    const winningCombinations = [
+      [0, 1, 2], [3, 4, 5], [6, 7, 8], // rows
+      [0, 3, 6], [1, 4, 7], [2, 5, 8], // columns
+      [0, 4, 8], [2, 4, 6] // diagonals
+    ];
+    const winnerText = document.querySelector('.winnerText')
+    let draw = true;
+
+    for (let combination of winningCombinations) {
+      const [a, b, c] = combination;
+      if (gameBoard[a] !== '' && gameBoard[a] === gameBoard[b] && gameBoard[b] === gameBoard[c]) {
+        winnerText.textContent = `Winner is Player ${gameBoard[a]}!`;
+        draw = false;
+        break;
+      }
+     
+    }
+
+    if (!gameBoard.includes('') && draw===true) {
+      winnerText.textContent = `Its a Draw. Yawn`;
+    }
+    
+
+    }
+  
+  
+ 
+  return {avoidSameSquare,gameBoard};
  })();
 
+ 
 
   gameBoardModule.avoidSameSquare()
+
+
 
 
 let displayControllerModule = (function() {
